03dca19a6407f73f73688011dd56271496279640
thiago-couto <thiagoferreira2508@gmail.com>
2025-05-01 12:42:07 -0300

    Update src.models.py

--- src.models.py
+++ src.models.py
@@ -10,14 +10,56 @@ print(df.head())
 
 # Dataset com regiões e atributos demográficos
 dataset_demografico = [
-    ['Sudeste', 'Alta Renda', 'Idade Jovem'],
-    ['Sul', 'Baixa Renda', 'Idade Idosa'],
-    ['Norte', 'Média Renda', 'Idade Adulto'],
-    ['Sudeste', 'Alta Renda', 'Idade Adulto'],
-    ['Sul', 'Baixa Renda', 'Idade Jovem']
+    dataset_demografico = [
+    ['Região', 'Faixa Etária', 'Faixa Salarial'],
+    ['Sudeste', '17-21', '0-2.000'],
+    ['Sudeste', '22-24', '2.001-6.000'],
+    ['Sudeste', '25-29', '6.001-12.000'],
+    ['Sudeste', '30-34', '12.001-16.000'],
+    ['Sudeste', '35-39', '16.001-20.000'],
+    ['Sudeste', '40-44', 'mais que 20.001'],
+    ['Sudeste', '45-49', 'mais que 20.001'],
+    ['Sudeste', '50-54', 'mais que 20.001'],
+    ['Sudeste', 'mais que 55', 'mais que 20.001'],
+    ['Sul', '17-21', '0-2.000'],
+    ['Sul', '22-24', '2.001-6.000'],
+    ['Sul', '25-29', '6.001-12.000'],
+    ['Sul', '30-34', '12.001-16.000'],
+    ['Sul', '35-39', '16.001-20.000'],
+    ['Sul', '40-44', 'mais que 20.001'],
+    ['Sul', '45-49', 'mais que 20.001'],
+    ['Sul', '50-54', 'mais que 20.001'],
+    ['Sul', 'mais que 55', 'mais que 20.001'],
+    ['Norte', '17-21', '0-2.000'],
+    ['Norte', '22-24', '2.001-6.000'],
+    ['Norte', '25-29', '6.001-12.000'],
+    ['Norte', '30-34', '12.001-16.000'],
+    ['Norte', '35-39', '16.001-20.000'],
+    ['Norte', '40-44', 'mais que 20.001'],
+    ['Norte', '45-49', 'mais que 20.001'],
+    ['Norte', '50-54', 'mais que 20.001'],
+    ['Norte', 'mais que 55', 'mais que 20.001'],
+    ['Nordeste', '17-21', '0-2.000'],
+    ['Nordeste', '22-24', '2.001-6.000'],
+    ['Nordeste', '25-29', '6.001-12.000'],
+    ['Nordeste', '30-34', '12.001-16.000'],
+    ['Nordeste', '35-39', '16.001-20.000'],
+    ['Nordeste', '40-44', 'mais que 20.001'],
+    ['Nordeste', '45-49', 'mais que 20.001'],
+    ['Nordeste', '50-54', 'mais que 20.001'],
+    ['Nordeste', 'mais que 55', 'mais que 20.001'],
+    ['Centro-Oeste', '17-21', '0-2.000'],
+    ['Centro-Oeste', '22-24', '2.001-6.000'],
+    ['Centro-Oeste', '25-29', '6.001-12.000'],
+    ['Centro-Oeste', '30-34', '12.001-16.000'],
+    ['Centro-Oeste', '35-39', '16.001-20.000'],
+    ['Centro-Oeste', '40-44', 'mais que 20.001'],
+    ['Centro-Oeste', '45-49', 'mais que 20.001'],
+    ['Centro-Oeste', '50-54', 'mais que 20.001'],
+    ['Centro-Oeste', 'mais que 55', 'mais que 20.001'],
 ]
 
-df_demografico = pd.DataFrame(dataset_demografico, columns=['Região', 'Renda', 'Idade'])
+df_demografico = pd.DataFrame(dataset_demografico, columns=['Região', 'Faixa salarial', 'Faixa etária'])
 
 # Dividir os dados em treino e teste (por exemplo, 70% treino, 30% teste)
 train_df, test_df = train_test_split(df_demografico, test_size=0.3, random_state=42)
